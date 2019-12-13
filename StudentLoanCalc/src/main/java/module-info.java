82b5d2d596192f48629f63d936bc5fcafb7ad7f7
honghaoran <honghaoran@10.0.0.102>
2019-12-13 00:10:41 -0500

    -

--- StudentLoanCalc/src/main/java/module-info.java
+++ StudentLoanCalc/src/main/java/module-info.java
@@ -1,11 +1,14 @@
 module StudentLoanCalc {
-	
-	exports app;
-	requires javafx.base;
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires javafx.media;
+ 
+ exports app;
+ requires javafx.base;
+ requires javafx.controls;
+ requires javafx.graphics;
+ requires javafx.fxml;
+ requires javafx.media;
+ requires poi;
+ 
+ opens pkgLogic;
 
-	opens app.controller to javafx.fxml;
+ opens app.controller to javafx.fxml;
 }
